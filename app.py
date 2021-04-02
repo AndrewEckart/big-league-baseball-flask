6f1a0eb62f82d6d8ea0aef8944b418a0f6e008e5
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-04-02 12:56:19 -0500

    Add team pages

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask
-import statsapi
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+from models.season import League, Season
+from models.team import Team
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+league = League(103)
+season = Season(year=2021, league=league)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+@app.route("/teams/<manager>")
+def team(manager: str):
+    team = Team(manager, season)
+    team.fetch_all_stats()
+    return render_template("team.html", team=team)
+
+
 if __name__ == '__main__':
-    # app.run()
-    # players = statsapi.get("sports_players", {"season": 2021})["people"]
-    # print(players[0])
-    gary_sanchez = statsapi.player_stat_data(596142)
-    print(gary_sanchez)
+    app.run()
