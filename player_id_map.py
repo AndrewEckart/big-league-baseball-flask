c67251279a78667b794774333132829a8c205758
Andrew Eckart <andrew.g.eckart@gmail.com>
2025-05-10 14:47:05 -0700

    Add Scott

--- player_id_map.py
+++ player_id_map.py
@@ -6,3 +6,8 @@ PLAYER_ID_MAP = pd.read_csv(path)
 MLBID_TO_NAME: dict[int, str] = {
     row.MLBID: row.MLBNAME for row in PLAYER_ID_MAP.itertuples()
 }
+
+# Fix-up some players missing from the SFBB list.
+MLBID_TO_NAME.update({
+    681343: "Shane Smith",
+})
