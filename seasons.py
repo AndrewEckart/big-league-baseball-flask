4276a9f334f12f2ed8f4e41372271bbc67ac86f6
Andrew Eckart <andrew.g.eckart@gmail.com>
2025-05-10 14:32:24 -0700

    Add Paula

--- seasons.py
+++ seasons.py
@@ -7,7 +7,7 @@ seasons: list[Season] = [
     Season(
         year=2025,
         # fmt: off
-        managers=["Andrew", "Justin", "Ron", "Myron"],
+        managers=["Andrew", "Justin", "Ron", "Myron", "Paula"],
         # fmt: on
         rules=Rules(
             num_reserve_hitters=4,
