a2dacdeb976bd941804d29a7c6d1b371e9c580f0
Andrew Eckart <andrew.g.eckart@gmail.com>
2025-05-10 13:37:23 -0700

    Update to Python 3.11; add Justin's team

--- seasons.py
+++ seasons.py
@@ -7,7 +7,7 @@ seasons: list[Season] = [
     Season(
         year=2025,
         # fmt: off
-        managers=["Andrew"],
+        managers=["Andrew", "Justin"],
         # fmt: on
         rules=Rules(
             num_reserve_hitters=4,
